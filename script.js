6482564e65e254c92c33f15cb5cf03946b4a7b69
Juanma Barcia <juanmanuelbarciagarcia@gmail.com>
2021-05-20 13:33:25 +0200

    mod1 script.js

--- script.js
+++ script.js
@@ -1,132 +1,195 @@
-const resp = {
-    q5001: "pn",
-    q5002: [true, false, false],
-    q5003: 14,
-    q5004: [false, true, false],
-    q5005: "16Sem",
-    q5006: [true, true, false]
-}
+  const questions = [{
+      name: 'dos',
+      label: '2+2',
+      answers: [
+          { label: 'cuatro', value: 4 },
+          { label: 'cinco', value: 5 },
+          { label: 'seis', value: 6 },
+          { label: 'siete', value: 7 },
+      ],
+      correct: 4
+  }, {
+      name: 'tres',
+      label: '3+3',
+      answers: [
+          { label: 'cuatro', value: 4 },
+          { label: 'cinco', value: 5 },
+          { label: 'seis', value: 6 },
+          { label: 'siete', value: 7 },
+      ],
+      correct: 6
+  }, {
+      name: 'cuatro',
+      label: '4+4',
+      answers: [
+          { label: 'ocho', value: 8 },
+          { label: 'cinco', value: 5 },
+          { label: 'seis', value: 6 },
+          { label: 'siete', value: 7 },
+      ],
+      correct: 8
+  }, {
+      name: 'cinco',
+      label: '5+5',
+      answers: [
+          { label: 'cuatro', value: 4 },
+          { label: 'diez', value: 10 },
+          { label: 'seis', value: 6 },
+          { label: 'siete', value: 7 },
+      ],
+      correct: 4
+  }, {
+      name: 'seis',
+      label: '6+6',
+      answers: [
+          { label: 'cuatro', value: 4 },
+          { label: 'doce', value: 12 },
+          { label: 'seis', value: 6 },
+          { label: 'siete', value: 7 },
+      ],
+      correct: 4
+  }, {
+      name: 'q5001',
+      label: '¿Qué cuerpo de seguridad gestiona la expedición del DNI?',
+      answers: [
+          { label: 'La Policía Nacional', value: "pn" },
+          { label: 'La Guardia civil', value: "gc" },
+          { label: 'La Policía Local', value: "pl" }
+      ],
+      correct: "pn"
+  }]
 
-function campoRelleno(arr, index) {
-    if (document.querySelector(`#${arr[index]} p`)) {
-        document.querySelector(`#${arr[index]} p`).remove()
-    }
-}
+  // ============================= codigo inicial =============================== //
 
-function campoVacio(arr, index) {
-    if (!document.querySelector(`#${arr[index]} p`)) {
-        let pElement = document.createElement("p");
-        let contenido = document.createTextNode("Por favor seleccione alguna respuesta");
-        pElement.appendChild(contenido);
-        document.querySelector(`#${arr[index]}`).appendChild(pElement);
-        let p = document.querySelector(`#${arr[index]} p`);
-        let attr = document.createAttribute("class");
-        attr.value = "campo_vacio";
-        p.setAttributeNode(attr);
-    }
-    document.getElementById(arr[index]).className = "default"
-}
+  const resp = {
+      q5001: "pn",
+      q5002: [true, false, false],
+      q5003: 14,
+      q5004: [false, true, false],
+      q5005: "16Sem",
+      q5006: [true, true, false]
+  }
 
-function correcto(arr, index) {
-    document.getElementById(arr[index]).className = "green"
-}
+  function campoRelleno(arr, index) {
+      if (document.querySelector(`#${arr[index]} p`)) {
+          document.querySelector(`#${arr[index]} p`).remove()
+      }
+  }
 
-function incorrecto(arr, index) {
-    document.getElementById(arr[index]).className = "red"
-}
+  function campoVacio(arr, index) {
+      if (!document.querySelector(`#${arr[index]} p`)) {
+          let pElement = document.createElement("p");
+          let contenido = document.createTextNode("Por favor seleccione alguna respuesta");
+          pElement.appendChild(contenido);
+          document.querySelector(`#${arr[index]}`).appendChild(pElement);
+          let p = document.querySelector(`#${arr[index]} p`);
+          let attr = document.createAttribute("class");
+          attr.value = "campo_vacio";
+          p.setAttributeNode(attr);
+      }
+      document.getElementById(arr[index]).className = "default"
+  }
 
-document.getElementById("quiz").addEventListener("submit", function(event) {
+  function correcto(arr, index) {
+      document.getElementById(arr[index]).className = "green"
+  }
 
-    event.preventDefault();
+  function incorrecto(arr, index) {
+      document.getElementById(arr[index]).className = "red"
+  }
 
-    let valRespuestas = document.querySelectorAll('[name]')
-    let arrNames = []
+  document.getElementById("quiz").addEventListener("submit", function(event) {
 
-    for (let i = 1; i < valRespuestas.length - 1; i++) {
-        if (!arrNames.includes(valRespuestas[i].name)) {
-            arrNames.push(valRespuestas[i].name)
-        }
-    }
-    // --------------------------- Validaciones --------------------------- //
+      event.preventDefault();
 
-    for (let j = 0; j < arrNames.length; j++) {
-        let valResp = document.querySelectorAll(`[name="${arrNames[j]}"]`)
-        let contadorResp = undefined
-        let arrResp = []
-        for (let i = 0; i < valResp.length; i++) {
+      let valRespuestas = document.querySelectorAll('[name]')
+      let arrNames = []
 
-            // -------------------------------------------- RADIO -------------------------------------------- //
-            if (valResp[i].type == "radio") {
-                if (valResp[i].checked) {
-                    contadorResp++
-                    campoRelleno(arrNames, j)
-                    if (valResp[i].value == resp[arrNames[j]]) {
-                        correcto(arrNames, j)
-                    } else {
-                        incorrecto(arrNames, j)
-                    }
-                }
-                if (contadorResp == undefined) {
-                    campoVacio(arrNames, j)
-                }
-            }
+      for (let i = 1; i < valRespuestas.length - 1; i++) {
+          if (!arrNames.includes(valRespuestas[i].name)) {
+              arrNames.push(valRespuestas[i].name)
+          }
+      }
+      // --------------------------- Validaciones --------------------------- //
 
-            // -------------------------------------------- NUMBER -------------------------------------------- //
+      for (let j = 0; j < arrNames.length; j++) {
+          let valResp = document.querySelectorAll(`[name="${arrNames[j]}"]`)
+          let contadorResp = undefined
+          let arrResp = []
+          for (let i = 0; i < valResp.length; i++) {
 
-            if (valResp[i].type == "number") {
-                if (!valResp[i].value) {
-                    campoVacio(arrNames, j)
-                } else {
-                    campoRelleno(arrNames, j)
-                    if (valResp[i].value == resp[arrNames[j]]) {
-                        correcto(arrNames, j)
-                    } else {
-                        incorrecto(arrNames, j)
-                    }
-                }
-            }
+              // -------------------------------------------- RADIO -------------------------------------------- //
+              if (valResp[i].type == "radio") {
+                  if (valResp[i].checked) {
+                      contadorResp++
+                      campoRelleno(arrNames, j)
+                      if (valResp[i].value == resp[arrNames[j]]) {
+                          correcto(arrNames, j)
+                      } else {
+                          incorrecto(arrNames, j)
+                      }
+                  }
+                  if (contadorResp == undefined) {
+                      campoVacio(arrNames, j)
+                  }
+              }
 
-            // ------------------------------------------- CHECKBOX ------------------------------------------- //
+              // -------------------------------------------- NUMBER -------------------------------------------- //
 
-            if (valResp[i].type == "checkbox") {
-                arrResp.push(valResp[i].checked)
-                if (valResp[i].checked) {
-                    contadorResp++
-                }
-                if (arrResp.toString() == resp[arrNames[j]].toString()) {
-                    contadorResp++
-                    campoRelleno(arrNames, j)
-                    correcto(arrNames, j)
-                } else {
-                    campoRelleno(arrNames, j)
-                    incorrecto(arrNames, j)
-                }
-                if (contadorResp == undefined) {
-                    campoVacio(arrNames, j)
-                }
-            }
+              if (valResp[i].type == "number") {
+                  if (!valResp[i].value) {
+                      campoVacio(arrNames, j)
+                  } else {
+                      campoRelleno(arrNames, j)
+                      if (valResp[i].value == resp[arrNames[j]]) {
+                          correcto(arrNames, j)
+                      } else {
+                          incorrecto(arrNames, j)
+                      }
+                  }
+              }
 
-            // ---------------------------------------- SELECT MULTIPLE ---------------------------------------- //
+              // ------------------------------------------- CHECKBOX ------------------------------------------- //
 
-            if (valResp[i].type == "select-multiple") {
-                for (let i = 0; i < valResp[0].length; i++) {
-                    arrResp.push(valResp[0][i].selected)
-                    if (valResp[0][i].selected) {
-                        contadorResp++
-                    }
-                    if (arrResp.toString() == resp[arrNames[j]].toString()) {
-                        contadorResp++
-                        campoRelleno(arrNames, j)
-                        correcto(arrNames, j)
-                    } else {
-                        campoRelleno(arrNames, j)
-                        incorrecto(arrNames, j)
-                    }
-                    if (contadorResp == undefined) {
-                        campoVacio(arrNames, j)
-                    }
-                }
-            }
-        }
-    }
-})
+              if (valResp[i].type == "checkbox") {
+                  arrResp.push(valResp[i].checked)
+                  if (valResp[i].checked) {
+                      contadorResp++
+                  }
+                  if (arrResp.toString() == resp[arrNames[j]].toString()) {
+                      contadorResp++
+                      campoRelleno(arrNames, j)
+                      correcto(arrNames, j)
+                  } else {
+                      campoRelleno(arrNames, j)
+                      incorrecto(arrNames, j)
+                  }
+                  if (contadorResp == undefined) {
+                      campoVacio(arrNames, j)
+                  }
+              }
+
+              // ---------------------------------------- SELECT MULTIPLE ---------------------------------------- //
+
+              if (valResp[i].type == "select-multiple") {
+                  for (let i = 0; i < valResp[0].length; i++) {
+                      arrResp.push(valResp[0][i].selected)
+                      if (valResp[0][i].selected) {
+                          contadorResp++
+                      }
+                      if (arrResp.toString() == resp[arrNames[j]].toString()) {
+                          contadorResp++
+                          campoRelleno(arrNames, j)
+                          correcto(arrNames, j)
+                      } else {
+                          campoRelleno(arrNames, j)
+                          incorrecto(arrNames, j)
+                      }
+                      if (contadorResp == undefined) {
+                          campoVacio(arrNames, j)
+                      }
+                  }
+              }
+          }
+      }
+  })
