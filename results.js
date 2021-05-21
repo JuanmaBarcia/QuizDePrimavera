4c3e498525c4147bed626d89fadeb8e95634a138
Juanma Barcia <juanmanuelbarciagarcia@gmail.com>
2021-05-21 18:48:24 +0200

    mod results.js

--- results.js
+++ results.js
@@ -1,11 +1,5 @@
-
 let leerRespuestas = JSON.parse(localStorage.getItem("respuestas"))
-
-console.log(leerRespuestas.pop().aciertos)
-
-
 let leerPreguntas = JSON.parse(localStorage.getItem("numero de preguntas"))
 
-
-document.getElementById("resultado").innerHTML=leerRespuestas.pop().aciertos
-document.getElementById("numeropreguntas").innerHTML=leerPreguntas
+document.getElementById("resultado").innerHTML = leerRespuestas.pop().aciertos
+document.getElementById("numeropreguntas").innerHTML = leerPreguntas
