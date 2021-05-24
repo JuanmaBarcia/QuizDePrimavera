4965dfae24c9e0cefe28c307aa900d171596d293
Juanma Barcia <juanmanuelbarciagarcia@gmail.com>
2021-05-24 19:36:20 +0200

    intento grafica con firebase 1

--- JS/home.js
+++ JS/home.js
@@ -1,33 +1,52 @@
+import { firebaseConfig } from './config.js'
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+let arrFecha = []
+let arrAciertos = []
+
+async function readResults() {
+    let data = await db
+        .collection("results")
+        .orderBy("fecha", "desc")
+        .limit(5)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                arrFecha.push(doc.data().fecha)
+                arrAciertos.push(doc.data().aciertos)
+            });
+        });
+}
+readResults()
+
 let resultados = JSON.parse(localStorage.getItem("respuestas"))
-let ultimosresultados = resultados.slice(resultados.length-5,resultados.length)
+let ultimosresultados = resultados.slice(resultados.length - 5, resultados.length)
 
-let fecha = []
-let aciertos = []
 
-for (let i = 0; i < ultimosresultados.length; i++) {
-    fecha.push(ultimosresultados[i].fecha)
-    aciertos.push(ultimosresultados[i].aciertos)
-}
+// for (let i = 0; i < ultimosresultados.length; i++) {
+//     fecha.push(ultimosresultados[i].fecha)
+//     aciertos.push(ultimosresultados[i].aciertos)
+// }
 
-  let pintarGrafica = (fecha, aciertos) => {
+let pintarGrafica = (fecha, aciertos) => {
     new Chartist.Line('.ct-chart', {
-    labels: fecha,
-    series: [aciertos],
-  },
-  {
-    high:10,
-    low: 0,
-    showArea: false,
-    charPadding: {
-      top: 35,
-      right: 15,
-      bottom: 25,
-      left: 5
-    },
-    axisY: {
-      onlyInteger: true,
-      
-    }
-  });
+        labels: fecha,
+        series: [aciertos],
+    }, {
+        high: 10,
+        low: 0,
+        showArea: false,
+        charPadding: {
+            top: 35,
+            right: 15,
+            bottom: 25,
+            left: 5
+        },
+        axisY: {
+            onlyInteger: true,
+
+        }
+    });
 }
-pintarGrafica(fecha, aciertos)
+pintarGrafica(arrFecha, arrAciertos)
